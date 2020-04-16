6e01c1ea1d40b29323a186c34cd5dc199ff3def6
alexlecco <alexlecco24@gmail.com>
2020-04-15 21:49:47 -0300

    response returns html

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
 http.createServer(function(req, res) {
-    res.write("Hello World");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h1>Hello World con HTML</h1>");
     res.end();
 }).listen(3000, function(){
     console.log("Servidor escuchando en puerto 3000")
